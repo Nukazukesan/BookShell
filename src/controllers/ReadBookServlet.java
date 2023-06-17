3b154cd0ca3ba9d59c6193fc81ac02a6ca6d6886
taro.kirameki <taro.kirameki@techacademy.jp>
2023-06-17 23:29:53 +0900

    「getAllBooksを追加」

--- src/controllers/ReadBookServlet.java
+++ src/controllers/ReadBookServlet.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Bookshell;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class ReadBookServlet
@@ -25,9 +30,14 @@ public class ReadBookServlet extends HttpServlet {    //本の登録画面
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Bookshell> books = em.createNamedQuery("getAllBooks", Bookshell.class).getResultList();
+        response.getWriter().append(Integer.valueOf(books.size()).toString());
+
+        em.close();
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
